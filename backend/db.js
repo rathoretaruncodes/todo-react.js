85d2d389dfa2f10b0e7f55b668aa4c68044be2c6
Tarun Pratap Singh Rathore <rathoretaruncodes@gmail.com>
2024-02-01 19:38:23 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //Mention mongo url in an ENV file, not like this.
 //Do not commit it to github
-mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect("mongodb+srv://rathoretaruncodes:FEVPR%401045n@cluster0.fdbe4fs.mongodb.net/");
 const todoSchema = mongoose.Schema({
     title: String,
     description: String,
